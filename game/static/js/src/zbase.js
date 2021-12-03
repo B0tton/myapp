bd6b2d6098a2033f64515483fecbe154a3787bb1
Botton <yh4@foxmail.com>
2021-12-03 14:55:31 +0800

    write game

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,8 @@
-class Mygame {
+export class Mygame {
     constructor(id) {
         this.id = id;
         this.$my_game = $('#' + id);
-        this.menu = new MyGameMenu(this);
+        // this.menu = new MyGameMenu(this);
         this.playground = new GamePlayground(this);
 
         this.start();
