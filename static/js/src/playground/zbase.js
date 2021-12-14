919ccffc0476f846ce69ad0d3715ddd77db46cb4
Botton <yh4@foxmail.com>
2021-12-14 21:41:12 +0800

    normalizing unit

--- static/js/src/playground/zbase.js
+++ static/js/src/playground/zbase.js
@@ -2,7 +2,10 @@ class GamePlayground {
     constructor(root) {
         this.root = root;
         this.$playground = $(`<div class=game-playground></div>`);
+        this.root.$my_game.append(this.$playground);
+
         this.hide();
+        this.root.$my_game.append(this.$playground);
 
         this.start();
     }
@@ -13,19 +16,36 @@ class GamePlayground {
     }
 
     start() {
+        let outer = this;
+        $(window).resize(function () {
+            outer.resize();
+        });
+    }
+
+    resize() {
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        let unit = Math.min(this.width / 16, this.height / 9);
+        this.width = unit * 16;
+        this.height = unit * 9;
+        this.scale = this.height;
+
+        if (this.game_map) this.game_map.resize();
     }
 
     show() { // 打开playground界面
         this.$playground.show();
-        this.root.$my_game.append(this.$playground)
+
+        this.resize();
+
         this.width = this.$playground.width();
         this.height = this.$playground.height();
         this.game_map = new GameMap(this);
         this.players = [];
-        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
+        this.players.push(new Player(this, this.width / 2 / this.scale, 0.5, 0.05, "white", 0.15, true));
 
         for (let i = 0; i < 5; i++) {
-            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+            this.players.push(new Player(this, this.width / 2 / this.scale, 0.5, 0.05, this.get_random_color(), 0.15, false));
         }
     }
 
